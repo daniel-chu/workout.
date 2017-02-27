d164118eb5b39c5084e382a241b7368c1cc50e7f
daniel-chu <chu.da@husky.neu.edu>
2017-02-27 18:46:09 -0500

    Made styling of add set selectors look better for small mobile devices.

--- static/js/app-pages/tracker.js
+++ static/js/app-pages/tracker.js
@@ -17,7 +17,15 @@ $(document).ready(function() {
     $weightOrDistSelector.on('change', function() {
         var value = $weightOrDistSelector.find(":selected").text();
         $('#weight-or-dist-input').attr('placeholder', value);
-    })
+        if (window.matchMedia('(max-width: 767px)').matches) {
+            if (value == 'distance*') {
+                $('button[data-id="weight-or-dist-selector"]').css('font-size', '11px');
+            }
+        }
+        if (value !== 'distance*') {
+            $('button[data-id="weight-or-dist-selector"]').css('font-size', '14px');
+        }
+    });
 
     $('#submit-set-info-button').on('click', function() {
         var workoutIdToAddTo = $(this).parents('#add-set-popup').data('workoutId');
